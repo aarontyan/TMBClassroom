6fd2f6aae6ae78135ac9fad61a9e4f73f5f2feca
naynoraa <aaron.t.yan@gmail.com>
2024-05-24 20:56:25 -0700

    Added route to get all classes and added address column to database

--- backend/backend.py
+++ backend/backend.py
@@ -4,27 +4,36 @@ import datetime as dt
 from pymongo import MongoClient
 from backend_helpers import *
 import pytz
+from bson.json_util import dumps, loads
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
+client = MongoClient("localhost", 27017)
 db = client["tmb-classroom"]
 collection = db["tmb-classroom"]
 time_zone = pytz.timezone("America/Chicago")
+
 
 @app.route("/")
 def hello_world():
     return "Hello"
 
-@app.route("/classes/current", methods=['GET'])
+
+@app.route("/classes/all", methods=["GET"])
+def get_all_classes():
+    response = collection.find({}, {"_id": 0})
+    return make_response(loads(dumps(response)), 200)
+
+
+@app.route("/classes/current", methods=["GET"])
 def get_current_classes():
-    # current_info = dt.datetime.now(tz = time_zone)
-    # current_time =  dt.time(current_info.hour, current_info(tz=time_zone).minute, tzinfo = time_zone)
-    current_info = dt.datetime(2024, 9, 2, tzinfo = time_zone)
-    current_time = dt.time(12, 0, tzinfo = time_zone)
-    entrees = collection.find({
-        "Start Date":{"$lt": current_info},
-        "End Date":{"$gt" : current_info}
-    })
+    current_info = dt.datetime.now(tz=time_zone)
+    current_time = dt.time(current_info.hour, current_info.minute, tzinfo=time_zone)
+    # current_info = dt.datetime(2024, 9, 2, tzinfo = time_zone)
+    # current_time = dt.time(12, 0, tzinfo = time_zone)
+    entrees = collection.find(
+        {"Start Date": {"$lt": current_info}, "End Date": {"$gt": current_info}},
+        {"_id": 0},
+    )
     filtered = []
     for entry in entrees:
         start_time_str = entry.get("Start")
@@ -33,7 +42,10 @@ def get_current_classes():
         if start_time_str and end_time_str:
             start_time = convertToTime(start_time_str)
             end_time = convertToTime(end_time_str)
-            if start_time < current_time and end_time > current_time and isCurrentDay(days):
-                del entry['_id']
+            if (
+                start_time < current_time
+                and end_time > current_time
+                and isCurrentDay(days)
+            ):
                 filtered.append(entry)
     return make_response(jsonify(filtered), 200)
--- backend/backend_helpers.py
+++ backend/backend_helpers.py
@@ -2,14 +2,20 @@ import pandas as pd
 import datetime as dt
 import pytz
 from pymongo import MongoClient
+from config import MAPS_KEY
+import requests
+import googlemaps
+import json
 
 time_zone = pytz.timezone("America/Chicago")
+
 
 # Returns the current day of the week
 def currentDay():
     curr_day = dt.datetime.today().weekday()
-    days  = ["M", "T", "W", "R", "F", "S", "U"]
+    days = ["M", "T", "W", "R", "F", "S", "U"]
     return days[curr_day]
+
 
 # Converts date string to Date object
 def convertToDate(date):
@@ -21,23 +27,28 @@ def convertToDate(date):
     day = int(split[2][0:-1])
     return dt.date(year, month, day)
 
+
 # Converts time to Time object
 def convertToTime(time):
     times = time.split(":")
-    return dt.time(int(times[0]), int(times[1]), int(times[2]), tzinfo = time_zone)
-    
-# Converts a datetime object to a time objecf
-def convertDTtoTime(datetime : dt.datetime):
-    return dt.time(datetime.hour, datetime.minute, tzinfo = time_zone)
+    return dt.time(int(times[0]), int(times[1]), int(times[2]), tzinfo=time_zone)
 
-def convertDTtoDate(datetime : dt.datetime):
+
+# Converts a datetime object to a time objecf
+def convertDTtoTime(datetime: dt.datetime):
+    return dt.time(datetime.hour, datetime.minute, tzinfo=time_zone)
+
+
+def convertDTtoDate(datetime: dt.datetime):
     return dt.date(datetime.year, datetime.day, datetime.minute)
+
 
 # Returns true if a class is happening on the current day
 def isCurrentDay(days):
     return currentDay() in days
 
-# Converts dataframe's columns for comparisons 
+
+# Converts dataframe's columns for comparisons
 def convertDfToDT(df):
     df["Start Date"] = df["Start Date"].apply(convertToDate)
     df["End Date"] = df["End Date"].apply(convertToDate)
@@ -45,3 +56,44 @@ def convertDfToDT(df):
     df["End"] = df["End"].apply(convertToTime)
     df["Correct Day"] = df["Days"].apply(isCurrentDay)
     return df
+
+
+# Uses Google Places API to find address of building
+def getAddressFromBuilding(building):
+    url = "https://places.googleapis.com/v1/places:searchText"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": MAPS_KEY,
+        "X-Goog-FieldMask": "places.displayName,places.formattedAddress",
+    }
+    data = {
+        "textQuery": building,
+        "pageSize": 10,
+        "locationBias": {
+            "circle": {
+                "center": {"latitude": 40.1061711, "longitude": -88.22693509999999},
+                "radius": 3.0,
+            }
+        },
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response and response.json():
+        return response.json()["places"][0]["formattedAddress"]
+    else:
+        return ""
+
+
+# Adds the address of every building as a new columns in the database
+def addAddressToDB():
+    client = MongoClient("localhost", 27017)
+    db = client["tmb-classroom"]
+    collection = db["tmb-classroom"]
+    documents = collection.find()
+    for doc in documents:
+        building = doc.get("Building")
+        if building:
+            new_address = getAddressFromBuilding(building)
+            collection.update_one(
+                {"_id": doc["_id"]}, {"$set": {"Address": new_address}}
+            )
